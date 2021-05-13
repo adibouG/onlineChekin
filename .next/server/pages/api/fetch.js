acc45ec370623f66903a3b1e5f3fdeb253e7d461
Henk van der Spek <henk@vanmezelf.nl>
2021-05-13 20:56:24 +0200

    Changed package name

---
+++ .next/server/pages/api/fetch.js
@@ -0,0 +1,36 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function() {
+var exports = {};
+exports.id = "pages/api/fetch";
+exports.ids = ["pages/api/fetch"];
+exports.modules = {
+
+/***/ "./pages/api/fetch.js":
+/*!****************************!*\
+  !*** ./pages/api/fetch.js ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ((req, res) => {\n  res.status(200).json({\n    name: 'Erik de Vries'\n  });\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jaGVja2luLWFwcC8uL3BhZ2VzL2FwaS9mZXRjaC5qcz9jZmE2Il0sIm5hbWVzIjpbInJlcSIsInJlcyIsInN0YXR1cyIsImpzb24iLCJuYW1lIl0sIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQ0EsR0FBRCxFQUFNQyxHQUFOLEtBQWM7QUFDM0JBLEtBQUcsQ0FBQ0MsTUFBSixDQUFXLEdBQVgsRUFBZ0JDLElBQWhCLENBQXFCO0FBQUVDLFFBQUksRUFBRTtBQUFSLEdBQXJCO0FBQ0QsQ0FGRCIsImZpbGUiOiIuL3BhZ2VzL2FwaS9mZXRjaC5qcy5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IChyZXEsIHJlcykgPT4ge1xuICByZXMuc3RhdHVzKDIwMCkuanNvbih7IG5hbWU6ICdFcmlrIGRlIFZyaWVzJyB9KVxufVxuIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/api/fetch.js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = (__webpack_exec__("./pages/api/fetch.js"));
+module.exports = __webpack_exports__;
+
+})();
