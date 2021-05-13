bfabb52c021a30281ecafcc8895051f7ff6ed563
Henk van der Spek <henk@vanmezelf.nl>
2021-05-13 23:19:19 +0200

    Call external api from within api route

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -1,3 +1,11 @@
-export default (req, res) => {
-  res.status(200).json({ name: 'World!' })
+const axios = require('axios');
+
+export default async (req, res) => {
+  await axios({
+      method: 'get',
+      url: 'https://api.vercel.com/www/user',
+      headers: { 'Authorization': `Bearer ${process.env.TOKEN}` }
+    })
+    .then(response => res.status(200).json({name: response.data.user.email}))
+    .catch(error => res.status(200).send(error));
 }
