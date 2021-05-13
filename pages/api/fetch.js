721ec51e2fce03bd7e0fa1c96aa9fab216153cde
Henk van der Spek <henk@vanmezelf.nl>
2021-05-13 22:13:06 +0200

    Better fetch

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -1,3 +1,3 @@
 export default (req, res) => {
-  res.status(200).json({ name: 'Erik de Vries' })
+  res.status(200).json({ name: 'World!' })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,23 @@
 import styles from '../styles/Home.module.css'
-import React, { useState, useEffect } from 'react'
+import useSWR from 'swr'
 import Spinner from 'react-bootstrap/Spinner'
 
-function Home() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [name, setName] = useState(null);
+const fetcher = url => fetch(url).then(res => res.json());
 
-  useEffect(() => {
-    fetch("api/fetch")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setName(result.name);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
+export default function Home() {
+  
+  const { data, error } = useSWR('/api/fetch', fetcher);
 
   const content = () => {
     if (error) {
       return `Error: ${error.message}`;
-    } else if (!isLoaded) {
-      return (<Spinner animation="border" role="status">
-          <span className="sr-only">Loading...</span>
-        </Spinner>);
+    } else if (!data) {
+      return <Spinner animation="border" role="status"/>;
     } else {
-      return `Hello ${name}`;
+      console.log(data);
+      return `Hello ${data.name}`;
     }
   };
 
   return <div className={styles.container}>{content()}</div>;
 }
-
-export default Home
