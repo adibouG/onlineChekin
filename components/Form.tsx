bfd3e8a94acd92f9a9b8d94453909e448d43253d
Henk van der Spek <henk@enzosystems.com>
2021-05-17 17:59:04 +0200

    WIP: start of first screen

--- components/Form.tsx
+++ components/Form.tsx
@@ -8,18 +8,10 @@ export function Form(props) {
     const { defaultValue } = props
 
     return (
-        <Card className={styles.card}>
-            <Card.Header>Enter your details</Card.Header>
-            <Card.Body>
-                <F>
-                    <F.Group controlId="formBasicEmail">
-                        <F.Label>Email address</F.Label>
-                        <F.Control id="email" aria-describedby="basic-addon3" defaultValue={defaultValue} />
-                        <F.Text className="text-muted">We'll never share your email with anyone else.</F.Text>
-                    </F.Group>
-                    <Button className="float-right" variant="primary" type="submit">Next</Button>         
-                </F>
-            </Card.Body>
-        </Card>
+        <div className={styles.screen}>
+            <div className={styles.card}>
+                <Button variant="primary" type="submit">Yes, start check-in</Button>
+            </div>
+        </div>
     )
 }
