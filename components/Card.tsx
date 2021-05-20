471330894c2ce2974644aadc99153180cf8af3ac
Henk van der Spek <henk@enzosystems.com>
2021-05-20 14:54:19 +0200

    Added media queries

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,21 +6,23 @@ export function Card(props) {
     const { supertitle, title, subtitle, buttonTitle } = props
     return (
         <div className={styles.card}>
-            <div className={styles.supertitle}>{supertitle}</div>
-            <div className={`${styles.title}, card-title`}>{title}</div>
-            <div className={styles.subtitle}>{subtitle}</div>
-            <Button 
-                variant="custom2" 
-                type="submit" 
-                className={styles.submit} 
-                size='lg' 
-                style={{
-                    backgroundColor: 'var(--button-background-color)',
-                    color: 'var(--button-color)'
-                }}
-            >
-                {buttonTitle}
-            </Button>
+            <div className={styles.grid}>
+                <div className={styles.supertitle}>{supertitle}</div>
+                <div className={`${styles.title} card-title`}>{title}</div>
+                <div className={styles.subtitle}>{subtitle}</div>
+                <Button 
+                    variant="custom2" 
+                    type="submit" 
+                    className={styles.submit} 
+                    size='lg' 
+                    style={{
+                        backgroundColor: 'var(--button-background-color)',
+                        color: 'var(--button-color)'
+                    }}
+                >
+                    {buttonTitle}
+                </Button>
+            </div>
         </div>
     )
 }
