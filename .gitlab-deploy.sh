6c77a427ea58ae769f08ba9ff4ab88ec9261bd0b
adrien gonzalez <adrien@enzosystems.com>
2021-05-27 17:19:02 +0200

    fix to pipeline scripts to include new packages

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,5 +6,5 @@ array=(${string//,/ })
 
 for i in "${!array[@]}" ; do
     echo "Deploy on ${array[i]}"
-    ssh ec2-user@${array[i]} "cd /home/ec2-user/gitTest/checkin/checkin && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin.git develop"
+    ssh ec2-user@${array[i]} "cd /home/ec2-user/gitTest/checkin/checkin && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin.git develop && npm install"
 done
