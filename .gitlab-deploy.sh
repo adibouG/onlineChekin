35b9c7b62f0ef1785e118ec6a24749ca4659cc86
adrien gonzalez <adrien@enzosystems.com>
2021-06-16 14:40:36 +0000

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,5 +6,5 @@ array=(${string//,/ })
 
 for i in "${!array[@]}" ; do
     echo "Deploy on ${array[i]}"
-    ssh ubuntu@${array[i]} "./Enzo/scripts/checkin/checkin_ssh_1.sh && cd /home/ec2-user/Enzo/checkin/repo/checkin/checkin && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin.git develop && npm install"
+    ssh gitdeploy@${array[i]} "../ubuntu/Enzo/scripts/checkin/checkin_ssh_1.sh && cd /home/ec2-user/Enzo/checkin/repo/checkin/checkin && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin.git develop && npm install"
 done
