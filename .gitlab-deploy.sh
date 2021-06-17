9f1e4e197c7093ddf85d717cac4e2a2b7ea6b9f6
adrien gonzalez <adrien@enzosystems.com>
2021-06-17 10:50:55 +0000

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,7 +6,5 @@ array=(${string//,/ })
 
 for i in "${!array[@]}" ; do
     echo "Deploy on ${array[i]}"
-    
-    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa gitdeploy@34.244.245.12" -i ~/.ssh/id_rsa_app ec2-user@10.0.0.47  "whoami &&  pwd"
-
+    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa $DEPLOY_USER_NAME@$DEPLOY_SERVER_URL" -i ~/.ssh/id_rsa_app ec2-user@$APP_SERVER_URL  "cd Enzo/checkin/repo/checkin/checkin/ && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin.git develop && npm install"
 done
