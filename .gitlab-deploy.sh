e8bacbe66d97668b7d68c1fabaa2bc51189109f6
adrien gonzalez <adrien@enzosystems.com>
2021-06-17 10:25:27 +0000

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -7,6 +7,6 @@ array=(${string//,/ })
 for i in "${!array[@]}" ; do
     echo "Deploy on ${array[i]}"
     
-    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i .ssh/id_rsa gitdeploy@34.244.245.12" -i .ssh/id_rsa2 ec2-user@10.0.0.47  "whoami &&  pwd"
+    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa gitdeploy@34.244.245.12" -i ~/.ssh/id_rsa_app ec2-user@10.0.0.47  "whoami &&  pwd"
 
 done
