700bac76397ef2a6a4f8dcc2b309701480b9de4a
adibouG <68852816+adibouG@users.noreply.github.com>
2023-09-13 11:26:37 +0200

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,6 +6,6 @@ array=(${string//,/ })
 
 for i in "${!array[@]}" ; do
      echo "Deploy on instance1"
-    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa ${4}@${5}" -i ~/.ssh/id_rsa_app ${3}@${i}  "cd Enzo/checkin/repo/checkin/checkin/ && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin.git develop && npm install"
+    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa ${4}@${5}" -i ~/.ssh/id_rsa_app ${3}@${i}  "cd Enzo/checkin/repo/checkin/checkin/ && git pull https://${1}:${2}@${6} && npm install"
  
 done
